4a51b1c38e866385e6e54030d4c11ec23f3698fd
Jose Sonhy <shimazaky1980@gmail.com>
2022-02-14 23:41:07 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 // selecting attributes from html
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]'
-const THUMBNAIL_LINK_SELECTOR = '[data-image-role="target"]'
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'
 
 // function to change the image and text
 function setDetails(imageURL, titleText) {
@@ -22,11 +22,22 @@ function setDetails(imageURL, titleText) {
 
 // function to retrieve and return the value of the data-image-url attribute
 function imageFromThumb(thumbnail) {
-    'use strict'
-    return thumbnail.getAttribute('data-image-url')
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
 }
 
 function titleFromThumb(thumbnail) {
     'use strict'
     return thumbnail.getAttribute('data-image-title')
+}
+
+/*
+    Brings the three other functions together for convenience, so
+    that you do not need to call them separately. It will accept a reference to a thumbnail
+    element and then call setDetails, passing in the values from calling
+    imageFromThumb and titleFromThumb.
+*/
+function setDetailsFromThumb(thumbnail) {
+    'use strict'
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail))
 }
